6eb37c615cb1176bb57ef69bd3785a218920217c
nignaco <43485395+nignaco@users.noreply.github.com>
2018-12-03 19:41:30 -0800

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,42 +1,40 @@
 library("shiny")
 library("dplyr")
 library("ggplot2")
-library("rsconnect")
-library("leaflet")
 state <- data("state")
 print(state.name)
 state_name <- state.name
-hospi <- read.csv("~/INFO/INFO201/cnn_project/data/hosp_income.csv", stringsAsFactors = FALSE)
+hospi <- read.csv("data/hospital_data_full.csv", na.strings = c("NA", "Not Available", "Results are not available for this reporting period"), stringsAsFactors = FALSE)
 hospi <- filter(hospi, Hospital.overall.rating != "Not Available")
-
 hospi$latitude <- as.numeric(hospi$latitude)
 hospi$longitude <- as.numeric(hospi$longitude)
 hospi = hospi[complete.cases(hospi), ]
+State <- data("State")
+state_name <- state.name
+print(state.abb)
+city_name <- unique(hospi$City)
+print(city_name)
+#Hosp <- as.data.frame(Hosp)
+
+
 ui <- fluidPage(
-  titlePanel("Income vs population hospital ratio"),
-  
+  titlePanel("Different types of hospital ownership by state"),
   sidebarLayout(
     sidebarPanel(
+      
       selectInput(
-        "states", 
-        "states",
-        state_name,
-        selected = "Alabama"
-      ),
-      radioButtons("ratings", label = h3("National Rating"), 
-                   choices = list("5", "4", "3", "2", "1"),
-                   selected = "5"),
-      hr(),
-      fluidRow(column(3, verbatimTextOutput("value")))
+        "cities", 
+        "cities",
+        city_name,
+        selected = "CHICAGO"
+      )
     ),
     mainPanel(
-      plotOutput("scatterplot"),
-      verbatimTextOutput("description_1"),
-      textOutput("diagram_title"),
-      leafletOutput("mymap"),
-      verbatimTextOutput("description")
+      plotOutput("distPlot"),
+      textOutput("text")
     )
   )
   
 )
+
 shinyUI(ui)
