a4f5f9f41d86278813c5ab3c9362d0da20c85e58
Nicole Lorenzo <32498793+nicolealorenzo@users.noreply.github.com>
2018-12-03 20:06:49 -0800

    comments added

--- ui.R
+++ ui.R
@@ -1,3 +1,4 @@
+## Get libraries
 library("shiny")
 library("dplyr")
 library("ggplot2")
@@ -6,29 +7,41 @@ library("leaflet")
 state <- data("state")
 print(state.name)
 state_name <- state.name
+
+## Read data
 hospi <- read.csv("~/INFO/INFO201/cnn_project/data/hosp_income.csv", stringsAsFactors = FALSE)
 hospi <- filter(hospi, Hospital.overall.rating != "Not Available")
 
+## Get needed information 
 hospi$latitude <- as.numeric(hospi$latitude)
 hospi$longitude <- as.numeric(hospi$longitude)
 hospi = hospi[complete.cases(hospi), ]
 city_name <- unique(hospi$City)
+
+## Shiny UI
 ui <- fluidPage(
   titlePanel("Income vs population hospital ratio"),
   
+  ## Sidebar
   sidebarLayout(
     sidebarPanel(
+      
+      ## Dropdown widget for figure 1 
       selectInput(
         "states", 
         "Figure 1: Select States",
         state_name,
         selected = "Alabama"
       ),
+      
+      ## Button widget for figure 2
       radioButtons("ratings", label = h3("National Rating"), 
                    choices = list("5", "4", "3", "2", "1"),
                    selected = "5"),
       hr(),
       fluidRow(column(3, verbatimTextOutput("value"))),
+      
+      ## Dropdown widget for figure 3
       selectInput(
         "cities", 
         "Figure 3: Select Cities",
@@ -36,6 +49,7 @@ ui <- fluidPage(
         selected = "CHICAGO"
       )
     ),
+    ## Display
     mainPanel(
       plotOutput("scatterplot"),
       verbatimTextOutput("description_1"),
@@ -45,7 +59,6 @@ ui <- fluidPage(
       plotOutput("distPlot"),
       textOutput("text")
     )
-  )
-  
+  )  
 )
 shinyUI(ui)
