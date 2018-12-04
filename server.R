eb1663440e1c8dcb4c883d2ec4f3d3cfbbca3f07
Nicole Lorenzo <32498793+nicolealorenzo@users.noreply.github.com>
2018-12-03 20:02:00 -0800

    added comments

--- server.R
+++ server.R
@@ -1,13 +1,17 @@
+## Get libraries
 library(shiny)
 library(dplyr)
 library(leaflet)
 library(dplyr)
 library(ggplot2)
 library("shiny")
+
+## Read in data 
 county_data <- read.csv("data/per_county_information.csv", stringsAsFactors = FALSE)
 county_data <- data.frame(county_data)
 states_unique <- unique(county_data$State)
 
+## Shiny server
 server <- function(input,output, session){
   dataset <- reactive({
     print(county_data)
@@ -18,22 +22,15 @@ server <- function(input,output, session){
   
   output$distPlot <- renderPlot({
     
-    ##get selected data from UI
+    ## Get selected data from UI
     hospi <- hospi %>%
       filter(City == input$cities) %>%
       group_by(Hospital.Ownership) %>%
       summarise(Hosp = n())
-    # print(input$states)
-    # print(hospi)
-    # 
-    # ##get percent per ownership
-    # hospi <- hospi %>%
-    #   group_by(Hospital.Ownership) %>%
-    #   summarise(Hosp = n())
-    
+   
     print(hospi$Hosp)
     
-    #graph
+    # Graph Figure 3
     ggplot(hospi, aes(x = '', y = Hosp , fill = Hospital.Ownership)) +
       geom_col(aes(fill = Hospital.Ownership), width = 1) + 
       geom_text(aes(label = hospi$Hosp), position = position_stack(vjust = 0.5)) +
@@ -42,13 +39,17 @@ server <- function(input,output, session){
     
   })
   
+  ## Print figure 1
   output$scatterplot <- renderPlot({
     ggplot(dataset(), aes(x=Income, y=population_hospital_ratio)) + geom_point() + ggtitle("Median Income vs Population to Hospital Ratio")
   })
   
+  ## Print title for figure 2 text
   output$diagram_title <- renderText({
     return("Hospitals in The US Based on Ratings and Area Income")
   })
+  
+  ## Print description for figure 2 text
   output$description <- renderText({
     return(paste("This map displays hospitals thougout the US based on a selected US National rating. the markers represent the givin areas income.", 
                  "Green represents high income (>$100,000)",
@@ -56,11 +57,13 @@ server <- function(input,output, session){
                  "Red represents low income (<= $35,000)", sep= "\n"))
   })
   
+  ## Description for figure 1
   output$description_1 <- renderText({
     return(paste("This scatterplot shows that generally for most states, counties with the best population to hospital ratio tends to be a county with income between $40,000 and $60,000",
                  "","", sep="\n"))
   })
   
+  ## Print map firgure 2
   output$mymap <- renderLeaflet({
     getColor <- function(hospi) {
       sapply(hospi$Income, function(Income) {
@@ -71,32 +74,23 @@ server <- function(input,output, session){
         }else if (Income > 35000.0 & Income <= 100000.0){
           "orange"
         } else {
-          "green"
-          
-          
-          
-          
-          
-          # } else if(Income > 35000 & Income <= 100000) {
-          #   "orange"
-          # } else {
-          #   "green"
+          "green"  
         } })
     }
     
+    ## markers for figure 2
     icons <- awesomeIcons(
       icon = 'ios-close',
       iconColor = 'black',
       #library = 'ion',
       markerColor = getColor(hospi)
     )
-    #
     
-    
-    # df <- data()
+    ## Filter data for figure 2
     hosp_filter <- hospi %>% 
       filter(Hospital.overall.rating == input$ratings )
     
+    ## Plots for figure 2
     m <- leaflet(data = hosp_filter) %>%
       addTiles() %>% 
       addAwesomeMarkers(lng = ~longitude,
@@ -107,7 +101,6 @@ server <- function(input,output, session){
                         popup = paste("Name:", hosp_filter$Hospital.Name, "<br>",
                                       "County:", hosp_filter$County.Name.x
                         ))
-    
     m
   })
 }
